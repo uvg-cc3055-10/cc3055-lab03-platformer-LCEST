e9870e8f8d85d36e5899b05a8b5b65e6e206aae7
LCEST <lcesturban10@gmail.com.com>
2018-04-21 22:45:15 -0600

    final

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,12 +11,16 @@ public class Character : MonoBehaviour {
     private float speed = 5f;
     private float jumpForce = 250f;
     private bool facingRight = true;
+    Animator anim;
+    AudioSource aud;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        anim = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
@@ -32,7 +36,8 @@ public class Character : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump")) {
             rb2d.AddForce(Vector2.up*jumpForce);
+            aud.Play();
         }
-
-	}
+        anim.SetFloat("Speed", Mathf.Abs(move));
+    }
 }
